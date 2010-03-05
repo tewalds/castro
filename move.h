158f1c9c7e7221e0a5292226f34d6fa47aadb191
Timo Ewalds <timo@timo-thinkpad.(none)>
2010-03-05 14:30:16 -0700

    Make MoveScore more efficient and buildable from Move

--- move.h
+++ move.h
@@ -6,7 +6,7 @@
 
 struct Move {
 	int8_t x, y;
-//	Move() { }
+
 	Move(int X = 0, int Y = 0) : x(X), y(Y) { }
 	Move(const Move & m){ x = m.x; y = m.y; }
 
@@ -19,9 +19,21 @@ struct Move {
 };
 
 struct MoveScore {
-	int x, y, score;
-//	MoveScore() { }
-	MoveScore(int X = 0, int Y = 0, int s = 0) : x(X), y(Y), score(s) { }
+	uint16_t x, y;
+	float score;
+
+	MoveScore(int X = 0, int Y = 0, float s = 0) : x(X), y(Y), score(s) { }
+	MoveScore(const Move & m, float s = 0) : x(m.x), y(m.y), score(s) { }
+
+	bool operator<(const MoveScore & b) const {
+		return (y == b.y ? x < b.x : y < b.y);
+	}
+	bool operator==(const MoveScore & b) const {
+		return (x == b.x && y == b.y);
+	}
+	bool operator==(const Move & b) const {
+		return (x == b.x && y == b.y);
+	}
 };
 
 #endif
