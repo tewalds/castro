5489f6a145bcbc2bcc92425324c370ea4652be28
Timo Ewalds <timo@timo-thinkpad.(none)>
2010-03-09 12:27:24 -0700

    Add a whole bunch of comparison operators

--- move.h
+++ move.h
@@ -10,39 +10,33 @@ struct Move {
 	Move(int X = 0, int Y = 0) : x(X), y(Y) { }
 	Move(const Move & m){ x = m.x; y = m.y; }
 
-	bool operator<(const Move & b) const {
-		return (y == b.y ? x < b.x : y < b.y);
-	}
-	bool operator==(const Move & b) const {
-		return (x == b.x && y == b.y);
-	}
+	bool operator< (const Move & b) const { return (y == b.y ? x <  b.x : y <  b.y); }
+	bool operator<=(const Move & b) const { return (y == b.y ? x <= b.x : y <= b.y); }
+	bool operator> (const Move & b) const { return (y == b.y ? x >  b.x : y >  b.y); }
+	bool operator>=(const Move & b) const { return (y == b.y ? x >= b.x : y >= b.y); }
+	bool operator==(const Move & b) const { return (x == b.x && y == b.y); }
+	bool operator!=(const Move & b) const { return (x != b.x || y != b.y); }
 };
 
 struct MoveScore {
-	uint16_t x, y;
+	int8_t x, y;
 	float score;
 
 	MoveScore(int X = 0, int Y = 0, float s = 0) : x(X), y(Y), score(s) { }
 	MoveScore(const Move & m, float s = 0) : x(m.x), y(m.y), score(s) { }
 
-	bool operator<(const MoveScore & b) const {
-		return (y == b.y ? x < b.x : y < b.y);
-	}
-	bool operator<(const Move & b) const {
-		return (y == b.y ? x < b.x : y < b.y);
-	}
-	bool operator>(const MoveScore & b) const {
-		return (y == b.y ? x > b.x : y > b.y);
-	}
-	bool operator>(const Move & b) const {
-		return (y == b.y ? x > b.x : y > b.y);
-	}
-	bool operator==(const MoveScore & b) const {
-		return (x == b.x && y == b.y);
-	}
-	bool operator==(const Move & b) const {
-		return (x == b.x && y == b.y);
-	}
+	bool operator< (const Move      & b) const { return (y == b.y ? x <  b.x : y <  b.y); }
+	bool operator< (const MoveScore & b) const { return (y == b.y ? x <  b.x : y <  b.y); }
+	bool operator<=(const Move      & b) const { return (y == b.y ? x <= b.x : y <= b.y); }
+	bool operator<=(const MoveScore & b) const { return (y == b.y ? x <= b.x : y <= b.y); }
+	bool operator> (const Move      & b) const { return (y == b.y ? x >  b.x : y >  b.y); }
+	bool operator> (const MoveScore & b) const { return (y == b.y ? x >  b.x : y >  b.y); }
+	bool operator>=(const Move      & b) const { return (y == b.y ? x >= b.x : y >= b.y); }
+	bool operator>=(const MoveScore & b) const { return (y == b.y ? x >= b.x : y >= b.y); }
+	bool operator==(const Move      & b) const { return (x == b.x && y == b.y); }
+	bool operator==(const MoveScore & b) const { return (x == b.x && y == b.y); }
+	bool operator!=(const Move      & b) const { return (x != b.x || y != b.y); }
+	bool operator!=(const MoveScore & b) const { return (x != b.x || y != b.y); }
 };
 
 #endif
