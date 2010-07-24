47e24546c44c3c2c939b1d6d37377a5f15192e58
Timo Ewalds <timo@ewalds.ca>
2010-07-24 21:50:28 +0800

    Add a basic types library with a few typedefs for simple types

---
+++ types.h
@@ -0,0 +1,36 @@
+
+
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+#include <stdint.h>
+
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned int   uint;
+typedef unsigned long  ulong;
+
+typedef  int8_t  i8;
+typedef uint8_t  u8;
+typedef  int16_t i16;
+typedef uint16_t u16;
+typedef  int32_t i32;
+typedef uint32_t u32;
+typedef  int64_t i64;
+typedef uint64_t u64;
+
+#if __WORDSIZE == 16
+	typedef  int16_t iword;
+	typedef uint16_t uword;
+#elif  __WORDSIZE == 32
+	typedef  int32_t iword;
+	typedef uint32_t uword;
+#elif __WORDSIZE == 64
+	typedef  int64_t iword;
+	typedef uint64_t uword;
+#else
+#error Unknown word size
+#endif
+
+#endif
+
