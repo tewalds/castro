113c7b9df00b5bc8c3dfe79be44b836608795834
Timo Ewalds <timo@ewalds.ca>
2010-09-26 07:23:23 +0900

    Allow ties to be handled directly instead of as a second run, though keep the option of assigning ties to one side or the other or doing two runs

--- solverpns.cpp
+++ solverpns.cpp
@@ -22,33 +22,38 @@ void SolverPNS::solve(Board board, double time, uint64_t memlimit){
 	int starttime = time_msec();
 
 	int turn = board.toplay();
-	int otherturn = (turn == 1 ? 2 : 1);
+	int otherturn = 3 - turn;
 
-	int ret1 = run_pns(board, otherturn, memlimit);
+	if(ties == 3){ //do both
+		assignties = otherturn;
+		int ret1 = run_pns(board, memlimit);
 
-	if(ret1 == 1){ //win
-		outcome = turn;
-	}else{
-		int ret2 = 0;
-		if(!timeout)
-			ret2 = run_pns(board, turn, memlimit);
-
-		if(ret2 == -1){
-			outcome = otherturn; //loss
+		if(ret1 == turn){ //win
+			outcome = turn;
 		}else{
-			if(ret1 == -1 && ret2 == 1) outcome = 0;          //tie
-			if(ret1 == -1 && ret2 == 0) outcome = -otherturn; //loss or tie
-			if(ret1 ==  0 && ret2 == 1) outcome = -turn;      //win or tie
-			if(ret1 ==  0 && ret2 == 0) outcome = -3;         //unknown
+			int ret2 = -3;
+			assignties = turn;
+			if(!timeout)
+				ret2 = run_pns(board, memlimit);
+
+			if(ret2 == otherturn){
+				outcome = otherturn; //loss
+			}else{
+				if(ret1 == otherturn && ret2 == turn) outcome = 0;          //tie
+				if(ret1 == otherturn && ret2 == -3  ) outcome = -otherturn; //loss or tie
+				if(ret1 == -3        && ret2 == turn) outcome = -turn;      //win or tie
+				if(ret1 == -3        && ret2 == -3  ) outcome = -3;         //unknown
+			}
 		}
+	}else{
+		assignties = ties; //could be 0, 1 or 2
+		outcome = run_pns(board, memlimit);
 	}
 
 	fprintf(stderr, "Finished in %d msec\n", time_msec() - starttime);
 }
 
-int SolverPNS::run_pns(const Board & board, int ties, uint64_t memlimit){ //1 = win, 0 = unknown, -1 = loss
-	assignties = ties;
-
+int SolverPNS::run_pns(const Board & board, uint64_t memlimit){ //1 = win, 0 = unknown, -1 = loss
 	if(root) delete root;
 	root = new PNSNode(0, 0, 1);
 	maxnodes = memlimit*1024*1024/sizeof(PNSNode);
@@ -66,15 +71,37 @@ int SolverPNS::run_pns(const Board & board, int ties, uint64_t memlimit){ //1 = 
 		}
 	}
 
-	if(root->phi == 0){
-		for(int i = 0; i < root->numchildren; i++)
-			if(root->children[i].delta == 0)
+//	printf("DRAW: %u, LOSS: %u\n", DRAW, LOSS);
+//	printf("root : %u, %u\n", root->phi, root->delta);
+//	for(PNSNode * i = root->children; i != root->children + root->numchildren; i++)
+//		printf("%i,%i : %u, %u\n", i->move.x, i->move.y, i->phi, i->delta);
+
+	if(root->phi == 0 && root->delta == LOSS){ //look for the winning move
+		for(int i = 0; i < root->numchildren; i++){
+			if(root->children[i].delta == 0){
 				bestmove = root->children[i].move;
-		return 1;
+				break;
+			}
+		}
+		return board.toplay();
 	}
-	if(root->delta == 0)
-		return -1;
-	return 0;
+	if(root->phi == 0 && root->delta == DRAW){ //look for the move to tie
+		for(int i = 0; i < root->numchildren; i++){
+			if(root->children[i].delta == DRAW){
+				bestmove = root->children[i].move;
+				break;
+			}
+		}
+		return 0;
+	}
+
+	if(root->delta == 0){ //loss
+		bestmove = M_NONE;
+		return 3 - board.toplay();
+	}
+
+	bestmove = M_UNKNOWN;
+	return -3;
 }
 
 bool SolverPNS::pns(const Board & board, PNSNode * node, int depth, uint32_t tp, uint32_t td){
@@ -109,12 +136,12 @@ bool SolverPNS::pns(const Board & board, PNSNode * node, int depth, uint32_t tp,
 				pd = 1; // phi & delta value
 			}
 
-			node->children[i] = PNSNode(*move).abval(abval, (board.toplay() == assignties), pd);
+			node->children[i] = PNSNode(*move).abval(abval, board.toplay(), assignties, pd);
 
 			i++;
 		}
 		for(; i < numnodes; i++) //add losses to the end so they aren't considered, only used when unique is true
-			node->children[i] = PNSNode(Move(M_NONE), 0, INF32);
+			node->children[i] = PNSNode(Move(M_NONE), 0, LOSS);
 
 		updatePDnum(node);
 
@@ -161,26 +188,35 @@ bool SolverPNS::pns(const Board & board, PNSNode * node, int depth, uint32_t tp,
 }
 
 bool SolverPNS::updatePDnum(PNSNode * node){
-	uint32_t min = INF32;
-	uint64_t sum = 0;
-
 	PNSNode * i = node->children;
 	PNSNode * end = node->children + node->numchildren;
 
+	uint32_t min = i->delta;
+	uint64_t sum = 0;
+
+	bool win = false;
 	for( ; i != end; i++){
+		win |= (i->phi == LOSS);
 		sum += i->phi;
 		if( min > i->delta)
 			min = i->delta;
 	}
 
-	if(sum >= INF32)
-		sum = INF32-1;
+	if(win)
+		sum = LOSS;
+	else if(sum >= INF32)
+		sum = INF32;
 
 	if(min == node->phi && sum == node->delta){
 		return false;
 	}else{
-		node->phi = min;
-		node->delta = sum;
+		if(sum == 0 && min == DRAW){
+			node->phi = 0;
+			node->delta = DRAW;
+		}else{
+			node->phi = min;
+			node->delta = sum;
+		}
 		return true;
 	}
 }
--- solverpns.h
+++ solverpns.h
@@ -13,7 +13,9 @@
 
 
 class SolverPNS : public Solver {
-	static const unsigned int INF32 = (1<<30)-1;
+	static const uint32_t LOSS  = (1<<30)-1;
+	static const uint32_t DRAW  = (1<<30)-2;
+	static const uint32_t INF32 = (1<<30)-3;
 public:
 
 	struct PNSNode {
@@ -34,13 +36,14 @@ public:
 			children = NULL;
 			numchildren = 0;
 		}
-		PNSNode & abval(int outcome, bool ties, int value = 1){
-			if(outcome == 1 || outcome == -1)
-				outcome = (ties ? 2 : -2);
+		PNSNode & abval(int outcome, int toplay, int assign, int value = 1){
+			if(assign && (outcome == 1 || outcome == -1))
+				outcome = (toplay == assign ? 2 : -2);
 
 			if(     outcome ==  0)   { phi = value; delta = value; }
-			else if(outcome ==  2)   { phi = INF32; delta = 0;     }
-			else /*(outcome == -2)*/ { phi = 0;     delta = INF32; }
+			else if(outcome ==  2)   { phi = LOSS;  delta = 0;     }
+			else if(outcome == -2)   { phi = 0;     delta = LOSS; }
+			else /*(outcome 1||-1)*/ { phi = 0;     delta = DRAW; }
 			return *this;
 		}
 		
@@ -70,6 +73,7 @@ public:
 	int   ab; // how deep of an alpha-beta search to run at each leaf node
 	bool  df; // go depth first?
 	float epsilon; //if depth first, how wide should the threshold be?
+	int   ties;    //0 handle ties, 1 assign p1, 2 assign p2, 3 do two searches, once assigned to each
 
 	bool timeout;
 
@@ -79,6 +83,7 @@ public:
 		ab = AB;
 		df = DF;
 		epsilon = eps;
+		ties = 0;
 
 		root = NULL;
 		reset();
@@ -107,7 +112,7 @@ public:
 	void solve(Board board, double time, uint64_t memlimit);
 
 //basic proof number search building a tree
-	int run_pns(const Board & board, int ties, uint64_t memlimit); //1 = win, 0 = unknown, -1 = loss
+	int run_pns(const Board & board, uint64_t memlimit); //-3 = unknown, 0 = tie, 1 = p1, 2 = p2
 	bool pns(const Board & board, PNSNode * node, int depth, uint32_t tp, uint32_t td);
 
 //update the phi and delta for the node
