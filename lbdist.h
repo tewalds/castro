1c08c47a2c8699710002c19942ce208df64a628a
Timo Ewalds <timo@ewalds.ca>
2010-09-30 11:34:52 +0900

    Update the distances to be even faster, since they no longer check all 6 directions.

--- lbdist.h
+++ lbdist.h
@@ -3,7 +3,7 @@
 /*
 Compute a rough lower bound on the number of additional moves needed to win given this position
 
-Done using floodfills from each edge/corner for each side
+Done using floodfills from each edge/corner for each side, only going in the 3 forward directions
 
 Increase distance when crossing an opponent virtual connection?
 Decrease distance when crossing your own virtual connection?
@@ -22,16 +22,17 @@ class LBDists {
 	struct MoveDist {
 		Move pos;
 		int dist;
+		int dir;
 
 		MoveDist() { }
-		MoveDist(Move p, int d) : pos(p), dist(d) { }
-		MoveDist(int x, int y, int d) : pos(Move(x,y)) , dist(d) { }
+		MoveDist(Move p, int d, int r)       : pos(p),         dist(d), dir(r) { }
+		MoveDist(int x, int y, int d, int r) : pos(Move(x,y)), dist(d), dir(r) { }
 		void print() const { printf("%i,%i: %i\n", pos.x, pos.y, dist); }
 	};
 
 	//a specialized priority queue
-	//inserted distances must be no more than maxvals larger than the current value
-	//new values must be the same as current or larger, never smaller
+	//distances must start at 0 or 1
+	//new values must be the same as current smallest, or larger but smaller than min + maxvals
 	class IntPQueue {
 		static const int maxvals = 4; //maximum number of distinct values that can be stored
 		MoveDist vals[maxvals][361];
@@ -77,10 +78,10 @@ class LBDists {
 	int & dist(int edge, int player, const Move & m) { return dist(edge, player, board->xy(m)); }
 	int & dist(int edge, int player, int x, int y)   { return dist(edge, player, board->xy(x, y)); }
 
-	void init(int x, int y, int edge, int player){
+	void init(int x, int y, int edge, int player, int dir){
 		int val = board->get(x, y);
 		if(val != 3 - player){
-			Q.push(MoveDist(x, y, (val == 0)));
+			Q.push(MoveDist(x, y, (val == 0), dir));
 			dist(edge, player, x, y) = (val == 0);
 		}
 	}
@@ -101,42 +102,42 @@ public:
 		int m = board->get_size()-1, e = board->get_size_d()-1;
 
 	//corner 0
-		init(0, 0, 0, 1); flood(0, 1);
-		init(0, 0, 0, 2); flood(0, 2);
+		init(0, 0, 0, 1, 3); flood(0, 1);
+		init(0, 0, 0, 2, 3); flood(0, 2);
 	//corner 1
-		init(m, 0, 1, 1); flood(1, 1);
-		init(m, 0, 1, 2); flood(1, 2);
+		init(m, 0, 1, 1, 4); flood(1, 1);
+		init(m, 0, 1, 2, 4); flood(1, 2);
 	//corner 2
-		init(e, m, 2, 1); flood(2, 1);
-		init(e, m, 2, 2); flood(2, 2);
+		init(e, m, 2, 1, 5); flood(2, 1);
+		init(e, m, 2, 2, 5); flood(2, 2);
 	//corner 3
-		init(e, e, 3, 1); flood(3, 1);
-		init(e, e, 3, 2); flood(3, 2);
+		init(e, e, 3, 1, 0); flood(3, 1);
+		init(e, e, 3, 2, 0); flood(3, 2);
 	//corner 4
-		init(m, e, 4, 1); flood(4, 1);
-		init(m, e, 4, 2); flood(4, 2);
+		init(m, e, 4, 1, 1); flood(4, 1);
+		init(m, e, 4, 2, 1); flood(4, 2);
 	//corner 5
-		init(0, m, 5, 1); flood(5, 1);
-		init(0, m, 5, 2); flood(5, 2);
+		init(0, m, 5, 1, 2); flood(5, 1);
+		init(0, m, 5, 2, 2); flood(5, 2);
 
 	//edge 0
-		for(int x = 1; x < m; x++)   { init(x,   0, 6,  1); } flood(6, 1);
-		for(int x = 1; x < m; x++)   { init(x,   0, 6,  2); } flood(6, 2);
+		for(int x = 1; x < m; x++)   { init(x,   0, 6,  1, 3+(x==1)); } flood(6, 1);
+		for(int x = 1; x < m; x++)   { init(x,   0, 6,  2, 3+(x==1)); } flood(6, 2);
 	//edge 1
-		for(int y = 1; y < m; y++)   { init(m+y, y, 7,  1); } flood(7, 1);
-		for(int y = 1; y < m; y++)   { init(m+y, y, 7,  2); } flood(7, 2);
+		for(int y = 1; y < m; y++)   { init(m+y, y, 7,  1, 4+(y==1)); } flood(7, 1);
+		for(int y = 1; y < m; y++)   { init(m+y, y, 7,  2, 4+(y==1)); } flood(7, 2);
 	//edge 2
-		for(int y = m+1; y < e; y++) { init(e,   y, 8,  1); } flood(8, 1);
-		for(int y = m+1; y < e; y++) { init(e,   y, 8,  2); } flood(8, 2);
+		for(int y = m+1; y < e; y++) { init(e,   y, 8,  1, 5+(y==m+1)); } flood(8, 1);
+		for(int y = m+1; y < e; y++) { init(e,   y, 8,  2, 5+(y==m+1)); } flood(8, 2);
 	//edge 3
-		for(int x = m+1; x < e; x++) { init(x,   e, 9,  1); } flood(9, 1);
-		for(int x = m+1; x < e; x++) { init(x,   e, 9,  2); } flood(9, 2);
+		for(int x = m+1; x < e; x++) { init(x,   e, 9,  1, 0+(x==e-1)); } flood(9, 1);
+		for(int x = m+1; x < e; x++) { init(x,   e, 9,  2, 0+(x==e-1)); } flood(9, 2);
 	//edge 4
-		for(int x = 1; x < m; x++)   { init(x, m+x, 10, 1); } flood(10, 1);
-		for(int x = 1; x < m; x++)   { init(x, m+x, 10, 2); } flood(10, 2);
+		for(int x = 1; x < m; x++)   { init(x, m+x, 10, 1, 1+(x==m-1)); } flood(10, 1);
+		for(int x = 1; x < m; x++)   { init(x, m+x, 10, 2, 1+(x==m-1)); } flood(10, 2);
 	//edge 5
-		for(int y = 1; y < m; y++)   { init(0,   y, 11, 1); } flood(11, 1);
-		for(int y = 1; y < m; y++)   { init(0,   y, 11, 2); } flood(11, 2);
+		for(int y = 1; y < m; y++)   { init(0,   y, 11, 1, 2+(y==m-1)); } flood(11, 1);
+		for(int y = 1; y < m; y++)   { init(0,   y, 11, 2, 2+(y==m-1)); } flood(11, 2);
 	}
 
 	void flood(int edge, int player){
@@ -144,8 +145,9 @@ public:
 
 		MoveDist cur;
 		while(Q.pop(cur)){
-			for(int i = 0; i < 6; i++){
-				MoveDist next(cur.pos + neighbours[i], cur.dist);
+			for(int i = 5; i <= 7; i++){
+				int nd = (cur.dir + i) % 6;
+				MoveDist next(cur.pos + neighbours[nd], cur.dist, nd);
 
 				if(board->onboard(next.pos)){
 					int pos = board->xy(next.pos);
