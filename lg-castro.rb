1fc8e475a4603b3665d35520607f6ba2046e9f56
Timo Ewalds <timo@ewalds.ca>
2010-10-28 14:56:54 -0600

    Update the little golem interface, ignoring swap and using 1 minute per boardsize

--- lg-castro.rb
+++ lg-castro.rb
@@ -11,8 +11,7 @@ class GTPClient
 		return @io.gets("\n\n")
 	end
 	def close
-		@io.close_write
-		@io.close_read
+		@io.close
 	end
 end
 
@@ -28,8 +27,9 @@ class Castro < LittleGolemInterface
 
 	def call_castro(size, moves)
 		gtp = GTPClient.new("./castro")
-		gtp.cmd("boardsize #{size}")
 		gtp.cmd("hguicoords")
+		gtp.cmd("swap 0")
+		gtp.cmd("boardsize #{size}")
 		case size
 			when 4 then gtp.cmd('player_params -g 1')
 			when 6 then gtp.cmd('player_params -R 50')
@@ -38,8 +38,8 @@ class Castro < LittleGolemInterface
 			when 9 then gtp.cmd('player_params -R 100 -p 1')
 			when 10 then gtp.cmd('player_params -R 130 -p 1')
 		end
-		gtp.cmd('time -r 0 -g 0 -m 90 -f 0')
-		gtp.cmd("playgame " + moves.join(' '))
+		gtp.cmd("time -r 0 -g 0 -m #{size*60} -f 0")
+		gtp.cmd("playgame " + moves.select{|m| m != 'swap'}.join(' '))
 		response = gtp.cmd('genmove')
 		gtp.cmd('quit')
 		sleep 1
@@ -50,8 +50,8 @@ class Castro < LittleGolemInterface
 		gameids.each do |g|
 			if (game = get_game(g))
 				size = game.scan(/SZ\[(.+?)\]/).flatten[0].to_i
-				moves = game.scan(/;[B|W]\[(.+?)\]/).flatten.map{|m| coord_HGF2GA(m, size) }
-				
+				moves = game.scan(/;[B|W]\[(.+?)\]/).flatten.map{|m| coord_HGF2GA(m, size) }.compact
+
 				self.log("Game #{g}, size #{size}: #{moves.join(' ')}")
 				newmove = self.call_castro(size, moves)
 				self.log("Game #{g}, size #{size}: response #{newmove}")
