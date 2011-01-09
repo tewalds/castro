e7224bab4f70958c9d2d278a353af3fd3a7d6f88
Timo Ewalds <timo@ewalds.ca>
2011-01-08 23:32:36 -0700

    Add an assert lib that lets you output a string, like the variables being tested

---
+++ assert2.h
@@ -0,0 +1,14 @@
+
+#ifndef _ASSERT2_H_
+#define _ASSERT2_H_
+
+#include <cassert>
+#include "string.h"
+
+#define assert2(expr, str) \
+  ((expr)								\
+   ? __ASSERT_VOID_CAST (0)						\
+   : __assert_fail ((string(__STRING(expr)) + "; " + (str)).c_str(), __FILE__, __LINE__, __ASSERT_FUNCTION))
+
+#endif
+
