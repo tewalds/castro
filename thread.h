0491858f4d61b551cd7929ffa4dd8b500e8ac177
Timo Ewalds <timo@ewalds.ca>
2011-06-24 19:33:45 -0600

    Add a spin lock

--- thread.h
+++ thread.h
@@ -111,6 +111,15 @@ public:
 	}
 };
 
+class SpinLock {
+	int taken;
+public:
+	SpinLock() : taken(0) { }
+	void lock()   { while(!trylock()) ; }
+	bool trylock(){ return !__sync_lock_test_and_set(&taken, 1); }
+	void unlock() { __sync_lock_release(&taken); }
+};
+
 //object wrapper around pthread rwlock
 class RWLock {
 	pthread_rwlock_t rwlock;
@@ -145,7 +154,7 @@ protected:
 public:
 	Mutex()  { pthread_mutex_init(&mutex, NULL); }
 	~Mutex() { pthread_mutex_destroy(&mutex); }
-	
+
 	int lock()    { return pthread_mutex_lock(&mutex); }
 	int trylock() { return pthread_mutex_trylock(&mutex); }
 	int unlock()  { return pthread_mutex_unlock(&mutex); }
