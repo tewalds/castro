b821ff2138783ebe52078957fd7329446de4c1b7
Timo Ewalds <timo@ewalds.ca>
2011-08-16 12:29:13 -0600

    Don't divide by 0

--- gtpplayer.cpp
+++ gtpplayer.cpp
@@ -101,8 +101,8 @@ GTPResponse HavannahGTP::gtp_move_stats(vecstr args){
 			continue;
 
 		s += move_str(child->move, true);
-		s += "," + to_str(child->exp.avg(), 2) + "," + to_str(child->exp.num());
-		s += "," + to_str(child->rave.avg(), 2) + "," + to_str(child->rave.num());
+		s += "," + to_str((child->exp.num()  ? child->exp.avg() : 0.0), 2) + "," + to_str(child->exp.num());
+		s += "," + to_str((child->rave.num() ? child->rave.avg() : 0.0), 2) + "," + to_str(child->rave.num());
 		s += "," + to_str(child->know);
 		if(child->outcome >= 0)
 			s += "," + won_str(child->outcome);
